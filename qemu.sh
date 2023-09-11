13c57be7d4f249b01c894616f13f41d44cd4c312
Krzysztof Krystian Jankowski <kj@p1x.in>
2023-09-12 00:54:04 +0200

    sound update + initial car added

--- qemu.sh
+++ qemu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-i386 -cpu 486 -m 32M -hda freedos-dev.img -fda p1x-dos-intro-01.img 
+qemu-system-i386 -cpu 486 -m 32M -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0 -hda freedos-dev.img -fda p1x-dos-intro-01.img 
